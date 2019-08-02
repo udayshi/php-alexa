502aa13b12f2a6c9e0eba7b60b8c6e16350d7ccc
Shiwakoti <uday.shiwakoti@mullenlowegroup.com>
2019-08-02 15:19:23 +0100

    Card example

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require('./vendor/autoload.php');
 $obj=new \USAlexa\Alexa();
 #Define function to handle request
 function launchIntentHandlerPhp(\USAlexa\Alexa $obj){
-    $obj->response->setResponse('Hello From PHP USAlexa.')->setCard('Card Title','Card Description');
+    $obj->response->setResponse('Hello From PHP USAlexa.')->setCard('Card Title','Card Description','https://avatars3.githubusercontent.com/u/1732894');
 }
 
 #Register intent and run
