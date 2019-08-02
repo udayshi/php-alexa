d8c17538572c77c6cb849b2f62fe73f211f9fefb
Shiwakoti <uday.shiwakoti@mullenlowegroup.com>
2019-08-02 15:01:20 +0100

    Card example

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $obj=new \USAlexa\Alexa();
 #Define function to handle request
 function launchIntentHandlerPhp(\USAlexa\Alexa $obj){
     $cardImg='https://avatars3.githubusercontent.com/u/1732894';
-    $obj->response->setResponse('Hello From PHP USAlexa.')->setCard('Hello','Hello card description',$cardImg);
+    $obj->response->setResponse('Hello From PHP USAlexa.')->setCard('Hello','Hello card description');
 }
 
 #Register intent and run
