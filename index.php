7100b85cb160a61d356a594d7f919e15b6721b3d
Shiwakoti <uday.shiwakoti@mullenlowegroup.com>
2019-08-02 15:02:55 +0100

    Card example

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require('./vendor/autoload.php');
 $obj=new \USAlexa\Alexa();
 #Define function to handle request
 function launchIntentHandlerPhp(\USAlexa\Alexa $obj){
-    $cardImg='https://avatars3.githubusercontent.com/u/1732894';
     $obj->response->setResponse('Hello From PHP USAlexa.')->setCard('Hello','Hello card description');
 }
 
